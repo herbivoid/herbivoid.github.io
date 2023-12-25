397c47262a088bda88b7af9ab8bbc6626ad69f48
herbivoid <154832432+herbivoid@users.noreply.github.com>
2023-12-25 21:57:17 +0200

    Add files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("menuButton").onclick = function() {
+  var menuContent = document.getElementById("menuContent");
+  if (menuContent.style.display === "block") {
+    menuContent.style.display = "none";
+  } else {
+    menuContent.style.display = "block";
+  }
+};
